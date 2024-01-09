4b29a06b9ef59dbe1fe22b1c4511382a6c496f80
Lal <2101348@cognizant.com>
2024-01-09 11:27:59 +0530

    added one method to add two numbers

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,8 +9,12 @@ public class App
     public static void main(String[] args) {
         System.out.println("Hello world");
         test();
+        add(10,20);
     }
     public static void test(){
         System.out.println("From test method");
+    }
+    public static void add(int a, int b){
+        System.out.println("Sum of "+ a +" & " + b + " = "+ (a+b));
     }
 }
