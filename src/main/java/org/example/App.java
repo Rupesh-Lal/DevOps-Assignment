3d35bcfa0c53b619353700e0e3404c88c6b4d185
Lal <2101348@cognizant.com>
2024-01-09 11:22:26 +0530

    added new method test

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,5 +8,9 @@ public class App
 {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        test();
+    }
+    public static void test(){
+        System.out.println("From test method");
     }
 }
