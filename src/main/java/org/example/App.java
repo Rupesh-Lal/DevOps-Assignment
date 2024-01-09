cc0ad566e46a0b71c4bf604e272d4b5e84f6c8c9
Lal <2101348@cognizant.com>
2024-01-09 11:33:02 +0530

    added sub mul & div method

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,11 +10,23 @@ public class App
         System.out.println("Hello world");
         test();
         add(10,20);
+        sub(20,10);
+        mul(10,20);
+        div(20,10);
     }
     public static void test(){
         System.out.println("From test method");
     }
     public static void add(int a, int b){
         System.out.println("Sum of "+ a +" & " + b + " = "+ (a+b));
+    }
+    public static void sub(int a, int b){
+        System.out.println("Sub of "+ a +" & " + b + " = "+ (a-b));
+    }
+    public static void mul(int a, int b){
+        System.out.println("Mul of "+ a +" & " + b + " = "+ (a*b));
+    }
+    public static void div(int a, int b){
+        System.out.println("Div of "+ a +" & " + b + " = "+ (a/b));
     }
 }
